a5edd94e4c850b7006b6da885191367a6a7d000c
virtual-principle-5400 <mnopprapun@gmail.com>
2021-03-04 21:58:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,11 @@ def introductin(name)
 end
 
 def introduction_with_language(name, language)
-  puts hi, my name is #{name} and I am learning to program in #{language}."
+  puts "hi, my name is #{name} and I am learning to program in #{language}."
   end
   
 def introduction_with_language(name, language = "Ruby")
-  puts hi, my name is #{name} and I am learning to program in #{language}."
+  puts "hi, my name is #{name} and I am learning to program in #{language}."
   end
 
 
